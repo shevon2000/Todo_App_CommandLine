d2cba75bd0e78e2a657d28a2333cc6b84e6ad484
shevon2000 <120797907+shevon2000@users.noreply.github.com>
2024-07-14 14:11:46 +0530

    capitalize() vs title()

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ prompt = "Enter a todo : "
 todos = []
 
 while True:
-    todo = input(prompt)
+    todo = input(prompt)        #clean the room
+    print(todo.capitalize())    #Clean the room
+    print(todo.title())         #Clean The Room
     todos.append(todo)
-    print(todos)
