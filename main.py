1d54dc16c9964aee5e564cbdfdcdfb23f4011582
shevon2000 <120797907+shevon2000@users.noreply.github.com>
2024-08-10 09:39:20 +0530

    add 'complete' feature to todo list

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit : ")
+    user_action = input("Type add, show, edit, complete or exit : ")
     user_action = user_action.strip()   #cut the spaces after the input
 
     match user_action:
@@ -10,13 +10,16 @@ while True:
             todos.append(todo)
         case 'show':
             for index, item in enumerate(todos):
-                row = f"{index}-{item}"  # f"string" - to manage the spaces between number and item
+                row = f"{index+1}-{item}"  # f"string" - to manage the spaces between number and item
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number -= 1
             new_todo = input("Enter new todo : ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number-1)
         case 'exit':
             break
 
